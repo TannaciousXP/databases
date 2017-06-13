93577ea34e2f0b8c9d0e58f7d8c67e442e4fd410
Hack Reactor Student <student@hackreactor.com>
2017-06-13 12:08:54 -0700

    Fix password issue in spec

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,17 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+const connection = mysql.createConnection({
+  user: 'root',
+  password: 'plantlife',
+  database: 'chat'
+});
 
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
 
